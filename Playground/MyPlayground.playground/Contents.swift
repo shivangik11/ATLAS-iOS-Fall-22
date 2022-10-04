f2a8dcf089d9947b2ac4705955802f1b165f139f
Shivangi Khandelwal <89596201+shivangik11@users.noreply.github.com>
2022-10-04 12:26:48 -0600

    Update Contents.swift

--- Playground/MyPlayground.playground/Contents.swift
+++ Playground/MyPlayground.playground/Contents.swift
@@ -103,4 +103,71 @@ math(4, 10)
 
 
 
+
+
+let apples = 31
+let oranges = 5
+let appleSummary = "I have \(apples) apples."
+let fruitSummary = "I have \(apples + oranges) pieces of fruit."
+print("I have \(apples + oranges) pieces of fruit.")
+
+print("hello, world!")
+
+let label = "The width is "
+let width = 94
+let widthLabel = label + String(width)
+
+var fruits = ["strawberries", "limes", "tangerines"]
+fruits[1] = "grapes"
+fruits.append("blueberries")
+print(fruits)
+
+
+let individualScores = [70, 43, 103, 87, 12]
+var teamScore = 0
+for score in individualScores {
+    if score > 50 {
+        teamScore += 10
+    } else {
+        teamScore += 1
+    }
+}
+print(teamScore)
+
+
+var optionalName: String? = "John Appleseed"
+var nickName: String? = "nil"
+var greeting = "Hello!"
+if let name = optionalName {
+    greeting = "Hello, \(name)"
+}
+else if let name = nickName{
+    greeting = "byebye, \(name)"
+}
+print(greeting)
+
+
+
+
+
+    let vegetable = ""
+    switch vegetable {
+    case "celery":
+        print("Add some raisins and make ants on a log.")
+    case "cucumber", "watercress":
+        print("That would make a good tea sandwich.")
+    case let x where x.hasSuffix("pepper"):
+        print("Is it a spicy \(x)?")
+    default:
+        print("Everything tastes good in soup.")
+    
+}
+
+
+
+
+
+
+
+
 //var greeting = "Hello, playground"
